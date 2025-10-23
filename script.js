273210287d5e2c778394b5e7f7870f0d4e7fb886
sanskruti <sanskrutishrivastri18@gmail.com>
2025-10-23 17:39:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple form alert
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+  this.reset();
+});
